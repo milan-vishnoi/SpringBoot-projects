8679dc2ea18f258a7822fab24e6a5706c3927fb6
Milan Vishnoi <milan.vish10@gmail.com>
2025-05-21 00:54:25 +0530

    Initial Commit

---
+++ sampleApp/src/test/java/com/miproject/sampleApp/SampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miproject.sampleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
