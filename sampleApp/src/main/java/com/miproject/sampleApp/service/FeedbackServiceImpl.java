24e4ade498a0e5812d536f452d12081809fc5cc9
Milan Vishnoi <milan.vish10@gmail.com>
2025-05-22 01:13:41 +0530

    Added the implementation of service layer

--- sampleApp/src/main/java/com/miproject/sampleApp/service/FeedbackServiceImpl.java
+++ sampleApp/src/main/java/com/miproject/sampleApp/service/FeedbackServiceImpl.java
@@ -1,5 +1,39 @@
 package com.miproject.sampleApp.service;
 
-public class FeedbackServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.miproject.sampleApp.repository.FeedbackRepository;
+import com.miproject.sampleApp.model.Feedback;
+import java.util.List;
+
+@Service
+public class FeedbackServiceImpl implements FeedbackService {
+
+    private final FeedbackRepository feedbackRepository;
+
+    @Autowired
+    public FeedbackServiceImpl(FeedbackRepository feedbackRepository) {
+        this.feedbackRepository = feedbackRepository;
+    }
+
+    @Override
+    public Feedback saveFeedback(Feedback feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    @Override
+    public List<Feedback> getAllFeedbacks() {
+        return feedbackRepository.findAll();
+    }
+
+    @Override
+    public Feedback getFeedbackById(Long feedbackId) {
+        return feedbackRepository.findById(feedbackId).orElse(null);
+    }
+
+    @Override
+    public void deleteFeedback(Long feedbackId) {
+        feedbackRepository.deleteById(feedbackId);
+    }
 
 }
