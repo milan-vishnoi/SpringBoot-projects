c37389c7e8d5942fbca3e73cc97610a102a7476a
Milan Vishnoi <milan.vish10@gmail.com>
2025-05-27 01:05:05 +0530

    Handled the MethodArgumentNotValidException

--- sampleApp/src/main/java/com/miproject/sampleApp/exception/GlobalExceptionHandler.java
+++ sampleApp/src/main/java/com/miproject/sampleApp/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,7 +15,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(FeedbackNotFoundException.class)
     public ResponseEntity<Map<String,Object>> handleFeedbackNotFoundException(FeedbackNotFoundException exception){
+        
         Map<String,Object> errorBody = new HashMap<>();
+
         errorBody.put("timestamp", LocalDateTime.now());
         errorBody.put("message",exception.getMessage());
         errorBody.put("status",HttpStatus.NOT_FOUND.value());
@@ -22,8 +25,21 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorBody,HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+
+        Map<String,String> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String,Object>> handleOtherExceptions(Exception exception){
+
         Map<String,Object> errorBody = new HashMap<>();
+
         errorBody.put("timestamp",LocalDateTime.now());
         errorBody.put("message",exception.getMessage());
         errorBody.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
